85107792dc0008789eff35f44692242a1bf3aaf2
Nikita Andryashin <yashiandr@gmail.com>
2024-02-27 11:28:20 +0300

    Добавил конфиг для роутера

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,6 +5,7 @@ import { useTheme } from 'app/providers/ThemeProvider/lib/useTheme';
 import { classNames } from 'shared/ui/classNames/classNames';
 import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
+import { AppRouter } from './providers/router';
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
@@ -15,12 +16,7 @@ const App = () => {
             <button onClick={toggleTheme}>toggle</button>
             <Link to={'/'}>Главная страница</Link>
             <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading....</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage />} />
-                    <Route path={'/'} element={<MainPage />} />
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 };
