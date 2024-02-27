4782a5e1e5ab8dcf608b1290a5d15ee424506fcc
Nikita Andryashin <yashiandr@gmail.com>
2024-02-27 03:48:29 +0300

    Add FSD struct

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
 import React, { Suspense, useContext, useState } from 'react';
 import './styles/index.scss'
 import { Route, Routes, Link } from "react-router-dom";
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { ThemeContext, Theme } from './theme/ThemeContext';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider/lib/useTheme';
+import { classNames } from 'shared/ui/classNames/classNames';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
@@ -18,8 +17,8 @@ const App = () => {
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading....</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />} />
-                    <Route path={'/'} element={<MainPageAsync />} />
+                    <Route path={'/about'} element={<AboutPage />} />
+                    <Route path={'/'} element={<MainPage />} />
                 </Routes>
             </Suspense>
         </div>
